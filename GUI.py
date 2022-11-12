c8e74d529062698a4e4ba4211837e7c7c78f8ff0
Nabil Johny <johnyn@mcmaster.ca>
2022-11-12 13:08:44 -0500

    runner

--- GUI.py
+++ GUI.py
@@ -1,7 +1,6 @@
 from tkinter import *
 import tkinter as tk
 import main.location as loc
-from ControlSystems.weather import Weather
 # from weather.weather import Weather
 
 class Monitor():
--- runner.py
+++ runner.py
@@ -1,17 +1,19 @@
 import main.location as loc
-from weather.weather import Weather
-# from GUI import *
 import GUI
+import ControlSystems.ControlSystem as cs
 
 def main():
+    weather = cs.WeatherSystem(51.5074, 0.1278)
+    weather.get_weather()
+    
     my_gui = GUI.Monitor()
 
     my_gui.display()
 
-    # print("Latitude: " + str(location.coords[0][0]) + ' Longitude: ' + str(location.coords[0][1]))
+    print("Latitude: " + str(loc.coords[0][0]) + ' Longitude: ' + str(loc.coords[0][1]))
     # w = Weather(longitude, latitude)
     # w.get_weather()
-
+    
 
 if __name__ == main():
     main()
