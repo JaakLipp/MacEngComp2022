9173fbfe0cfa05e09dcea8c1682edd6eab3d9841
JaakLipp <lippert.22j@gmail.com>
2022-11-12 15:47:13 -0500

    documentation

--- ControlSystems/runWeather.py
+++ ControlSystems/runWeather.py
@@ -1,7 +1,7 @@
 import ControlSystem as cs
 
 def main():
-    #runs the weather system
+    #runs the weather system, this is purely for testing
     weather = cs.WeatherSystem(51.5074, 0.1278)
     weather.get_weather()
 
